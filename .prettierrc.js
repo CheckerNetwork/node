fa036b19df27ea46b08fc96954a28b5716a80155
Miroslav Bajtoš <oss@bajtos.net>
2025-04-07 10:00:39 +0200

    setup prettier-plugin-jsdoc
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,6 @@
 /**
- * @see https://prettier.io/docs/configuration
  * @typedef {import('prettier').Config} Config
+ * @see https://prettier.io/docs/configuration
  */
 
 /** @type {Config} */
@@ -10,8 +10,16 @@ export default {
     // See https://github.com/electrovir/prettier-plugin-multiline-arrays?tab=readme-ov-file#compatibility
     'prettier-plugin-packagejson',
     'prettier-plugin-multiline-arrays',
+    'prettier-plugin-jsdoc',
   ],
-  multilineArraysWrapThreshold: 1,
+
+  // built-in options
   semi: false,
   singleQuote: true,
+
+  // prettier-plugin-multiline-arrays
+  multilineArraysWrapThreshold: 1,
+
+  // prettier-plugin-jsdoc
+  tsdoc: true,
 }
